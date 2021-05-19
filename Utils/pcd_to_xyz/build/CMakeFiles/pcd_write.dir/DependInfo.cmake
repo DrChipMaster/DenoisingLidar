b0c1f0cc01088af9d46ea76725db6ce2ab935135
DrChipMaster <andrecampos1998@gmail.com>
2021-05-19 05:26:10 -0700

    yes

--- Utils/pcd_to_xyz/build/CMakeFiles/pcd_write.dir/DependInfo.cmake
+++ Utils/pcd_to_xyz/build/CMakeFiles/pcd_write.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/andre/pcd_to_xyz/main.cpp" "/home/andre/pcd_to_xyz/build/CMakeFiles/pcd_write.dir/main.cpp.o"
+  "/home/andre/DenoisingLidar/Utils/pcd_to_xyz/main.cpp" "/home/andre/DenoisingLidar/Utils/pcd_to_xyz/build/CMakeFiles/pcd_write.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- Utils/pcd_to_xyz/main.cpp
+++ Utils/pcd_to_xyz/main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 int
 main (int argc, char** argv)
 {
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZI>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZI>);
 
-  if (pcl::io::loadPCDFile<pcl::PointXYZ> (argv[1], *cloud) == -1) //* load the file
+  if (pcl::io::loadPCDFile<pcl::PointXYZI> (argv[1], *cloud) == -1) //* load the file
   {
     PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
     return (-1);
@@ -23,12 +23,15 @@ main (int argc, char** argv)
   name_y.append("_y.txt");
   string name_z = name;
   name_z.append("_z.txt");
-  name.append(".xyz");
+  string name_i = name;
+  name_i.append("_i.txt");
+  name.append(".xyzi");
 
   ofstream file(name);
   ofstream file_x(name_x);
   ofstream file_y(name_y);
   ofstream file_z(name_z);
+  ofstream file_i(name_i);
 
   std::cout << "Loaded "
             << cloud->width * cloud->height
@@ -38,7 +41,8 @@ main (int argc, char** argv)
   {
     std::cout << "    " << point.x
               << " "    << point.y
-              << " "    << point.z << std::endl;
+              << " "    << point.z
+              << " "<<point.intensity << std::endl;
 
     file << point.x<<" "<<point.y<<" "<<point.z<<"\n";
     std::stringstream aux_x;
@@ -46,10 +50,13 @@ main (int argc, char** argv)
     std::stringstream aux_y;
       aux_y << std::hex << (int16_t) (point.y*100);
     std::stringstream aux_z;
-      aux_z << std::hex << (int16_t) (point.z*100);  
+      aux_z << std::hex << (int16_t) (point.z*100); 
+    std::stringstream aux_i;
+      aux_i << std::hex << (int16_t) (point.intensity);  
     file_x <<aux_x.str() << "\n";
     file_y <<aux_y.str() << "\n";
     file_z << aux_z.str() << "\n";
+    file_i << aux_i.str() << "\n";
 
   }
     
