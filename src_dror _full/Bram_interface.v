b58fa9162284a6c2923d8c936d3529562ace57bf
Andre <andrecampos1998@gmail.com>
2021-07-02 13:04:04 +0100

    Update

--- src_dror _full/Bram_interface.v
+++ src_dror _full/Bram_interface.v
@@ -21,24 +21,26 @@
 
 
 module Bram_interface #(parameter N = 16,
-                    DISTANCE_MODULES = 8,
-                    CORE_NUMBER = 8)
+                    DISTANCE_MODULES = 2,
+                    CORE_NUMBER = 32,
+                    BUS_SIZE =32
+                    )
                    (input wire clock,
                     output reg[31:0] addr_x,//Connectio to bram containing x
-                    output reg[127:0] write_in_x,
-                    input wire[127:0] read_out_x,
+                    output reg[BUS_SIZE-1:0] write_in_x,
+                    input wire[BUS_SIZE-1:0] read_out_x,
                     output reg en_x,
                     output reg  rst_x,
                     output reg[15:0] we_x,
                     output reg[31:0] addr_y,//Connectio to bram containing y
-                    output reg[127:0] write_in_y,
-                    input wire[127:0] read_out_y,
+                    output reg[BUS_SIZE-1:0] write_in_y,
+                    input wire[BUS_SIZE-1:0] read_out_y,
                     output reg en_y,
                     output reg  rst_y,
                     output reg[15:0] we_y,
                     output reg[31:0] addr_z,//Connectio to bram containing x
-                    output reg[127:0] write_in_z,
-                    input wire[127:0] read_out_z,
+                    output reg[BUS_SIZE-1:0] write_in_z,
+                    input wire[BUS_SIZE-1:0] read_out_z,
                     output reg en_z,
                     output reg  rst_z,
                     output reg[15:0] we_z,
@@ -68,7 +70,8 @@ reg read_fifo;
 reg cache_updated;
 
 
-
+reg [N-1:0] core_cache_status;
+reg [N-1:0] node_cache_status;
 
 
 
@@ -84,7 +87,6 @@ reg[N-1:0] feeder_pos;
 
 reg cache_ready;
 integer i,k;
-reg[4:0] test;
 
 always @(posedge clock)
 begin
@@ -93,58 +95,81 @@ begin
             if(update_cache )
             begin
                 pause <=1;
-                addr_x <= (point_pos)*2-1;
-                addr_y <=(point_pos)*2-1;
-                addr_z <= (point_pos)*2-1;
+                addr_x <= (point_pos+1)*2;
+                addr_y <=(point_pos+1)*2;
+                addr_z <= (point_pos+1)*2;
                 en_x <=1;
                 en_y <=1;
                 en_z <=1;
             end
             else
             begin
-         if ((feeder_pos) > point_cloud_size )
+        if(node_cache_status>=DISTANCE_MODULES)
+            begin
+                node_cache_status <=0;
+                pause <=0;
+                clear_cache <=1;
+            end
+        else
+        begin
+         if ((feeder_pos) > point_cloud_size && Controller_done ==0)
                 begin
                     feeder_pos <= 0;
                 end
                 else
-                    feeder_pos <= feeder_pos + 15;
-        addr_x <= feeder_pos;
-        addr_y <= feeder_pos;
-        addr_z <= feeder_pos;
-        en_x <=1;
-        en_y <=1;
-        en_z <=1;
-         cache_feeder_x <=  read_out_x;
-         cache_feeder_y <=  read_out_y;
-         cache_feeder_z <=  read_out_z;
-          pause <= 0;
-          cache_updated<=0;
-          reset<=0;
-    end
-    end
-    
-    else if(state ==1)
-    begin
-        
-        cache_x =0;
-        cache_y =0;
-        cache_z =0;
-        test = point_pos&(CORE_NUMBER-1);
-        
-        for ( i=0;i<CORE_NUMBER-test; i = i +1 ) begin
-            cache_x = cache_x +(read_out_x[(i+test+1)*N-1 -:N]<<(i*N));
-            cache_y = cache_y +(read_out_y[(i+test+1)*N-1 -:N]<<(i*N));
-            cache_z = cache_z +(read_out_z[(i+test+1)*N-1 -:N]<<(i*N));
+                    feeder_pos <= feeder_pos + BUS_SIZE/N;
+
+
+        if(clear_cache ==1)
+        begin
+            cache_feeder_x<=0;
+            cache_feeder_y<=0;
+            cache_feeder_z<=0;
+            clear_cache<=0;  
+            pause <=1;
         end
-        addr_x <= point_pos*2+15;
-        addr_y <= point_pos*2+15;
-        addr_z <= point_pos*2+15;
+        else begin
+            addr_x <= feeder_pos;
+            addr_y <= feeder_pos;
+            addr_z <= feeder_pos;
+            en_x <=1;
+            en_y <=1;
+            en_z <=1;
+            cache_feeder_x <=  cache_feeder_x+(read_out_x<<(16*(node_cache_status)));
+            cache_feeder_y <=  cache_feeder_y+(read_out_y<<(16*(node_cache_status)));
+            cache_feeder_z <=  cache_feeder_z+(read_out_z<<(16*(node_cache_status)));
+            pause <= 1;
+            node_cache_status <= node_cache_status +BUS_SIZE/N;
+            cache_updated<=0;  
+    end
+    end
+    end
+    end
+    else if(state ==1)
+    begin  
+        //cache_x =0;
+        //cache_y =0;
+        //cache_z =0;
+        //test = point_pos&(CORE_NUMBER-1);
+        
+        //for ( i=0;i<CORE_NUMBER-test; i = i +1 ) begin
+          //  cache_x = cache_x +(read_out_x[(i+test+1)*N-1 -:N]<<(i*N));
+          //  cache_y = cache_y +(read_out_y[(i+test+1)*N-1 -:N]<<(i*N));
+          //  cache_z = cache_z +(read_out_z[(i+test+1)*N-1 -:N]<<(i*N));
+        //end
+        cache_x <= read_out_x;
+        cache_y <= read_out_y;
+        cache_z <= read_out_z;
+        addr_x <= point_pos+BUS_SIZE/N +1;
+        addr_y <= point_pos+BUS_SIZE/N +1;
+        addr_z <= point_pos+BUS_SIZE/N +1;
+        core_cache_status <= 2;
         we_z <=0;
         we_y <= 0;
         we_x <= 0;
-        pause <=0;
+        //pause <=0;
         reset<=0;
-        cache_updated <=1;
+        //cache_updated <=1;
 
     end
     else  if(state == 3)
@@ -156,9 +181,9 @@ begin
         we_x <= 16'hffff;
         if(outlier_from_fifo != 0)
         begin
-        addr_x <= outlier_from_fifo*2-1;
-        addr_y <= outlier_from_fifo*2-1;
-        addr_z <= outlier_from_fifo*2-1;
+        addr_x <= outlier_from_fifo;
+        addr_y <= outlier_from_fifo;
+        addr_z <= outlier_from_fifo;
         end
         write_in_x <= 0;
         write_in_y <= 0;
@@ -182,13 +207,16 @@ begin
             point_cloud_size <= read_out_x[31:0];
             write_in_y[31:0] <=0;
             we_y <= 16'h000f;
-            addr_x <= (point_pos)*2;
-            addr_y <=(point_pos)*2;
-            addr_z <= (point_pos)*2;
+            addr_x <= (point_pos+1);
+            addr_y <=(point_pos+1);
+            addr_z <= (point_pos+1);
         end
         else begin
         point_cloud_size<=0;
         feeder_pos <=0;
+        node_cache_status <=0;
+        core_cache_status <=0;
+        pause <=1;
         end
     end
     else if(state==4)begin
