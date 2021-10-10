596aaa39eb5c0a591136509481f60d8a7f4431ff
DrChipMaster <andrecampos1998@gmail.com>
2021-10-10 15:10:54 -0700

    new script

---
+++ Utils/scripts/start_alfa_dvc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export ROS_IP=192.168.1.90
+echo "Exported ROS_IP -> $ROS_IP"
+export ROS_MASTER_URI=http://192.168.1.89:11311/
+echo "Exported ROS_MASTER_URI -> $ROS_MASTER_URI"
+source devel/setup.sh
+echo "Sourced devel/setup"
+rosrun alfa_dvc alfa_dvc
+
