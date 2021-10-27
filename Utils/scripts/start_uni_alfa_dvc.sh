62ab38e69635bf79ef9779db8ae5949b09f0dc5b
DrChipMaster <andrecampos1998@gmail.com>
2021-10-27 10:10:49 -0700

    pls dont

--- Utils/scripts/start_uni_alfa_dvc.sh
+++ Utils/scripts/start_uni_alfa_dvc.sh
@@ -3,7 +3,7 @@ export ROS_IP=192.168.197.6
 echo "Exported ROS_IP -> $ROS_IP"
 export ROS_MASTER_URI=http://192.168.197.60:11311/
 echo "Exported ROS_MASTER_URI -> $ROS_MASTER_URI"
-source devel/setup.sh
+source /home/andre/catkin_ws/devel/setup.sh
 echo "Sourced devel/setup"
 rosrun alfa_dvc alfa_dvc
 
